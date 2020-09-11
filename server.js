89b26b66b18efa78214ef9f5aeb202d216901eee
jundic <jundi_chen@sfu.ca>
2020-09-10 21:18:54 -0700

    a

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const router = require("./router");
 const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
